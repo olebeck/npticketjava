c242de970c4ac2665b990ca4cb28496c836767a4
Li <li@silica.codes>
2024-05-18 23:46:46 +0000

    plz consistent

--- src/main/java/yuv/pink/npticket/Entitlement.java
+++ src/main/java/yuv/pink/npticket/Entitlement.java
@@ -7,4 +7,13 @@ public class Entitlement {
     public int type;
     public int remainingCount;
     public int consumedCount;
+
+    public Entitlement(String entitlementID, long createdDate, long expiredDate, int type, int remainingCount, int consumedCount) {
+        this.entitlementID = entitlementID;
+        this.createdDate = createdDate;
+        this.expiredDate = expiredDate;
+        this.type = type;
+        this.remainingCount = remainingCount;
+        this.consumedCount = consumedCount;
+    }
 }
